f039c0186ee1526e9284916fcb79575e74011ae3
HiMyNameIsIlNano <kappei84@gmail.com>
2019-02-08 22:18:46 +0100

    Deleted Old Files;

--- ImageReader.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-import pytesseract as tess
-
-pytesseract.pytesseract.tesseract_cmd = r'/usr/local/lib/python2.7/site-packages/pytesseract/pytesseract.py'
-
-print tess.image_to_string(Image.open('/home/Developer/Downloads/password.png'), lang='eng')
